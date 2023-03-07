86e88bb98874b0b0fbf08f8e46c93a69b37efc0c
somyungsub <gkdldy5@gmail.com>
2023-03-07 23:12:51 +0900

    security - jwt-tutorial add

---
+++ security/oauth/jwt-tutorial/src/main/java/study/springboot/security/oauth/jwttutorial/controller/UserController.java
@@ -0,0 +1,43 @@
+package study.springboot.security.oauth.jwttutorial.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import study.springboot.security.oauth.jwttutorial.dto.UserDto;
+import study.springboot.security.oauth.jwttutorial.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+  @PostMapping("/test-redirect")
+  public void testRedirect(HttpServletResponse response) throws IOException {
+    response.sendRedirect("/api/user");
+  }
+
+  @PostMapping("/signup")
+  public ResponseEntity<UserDto> signup(@Valid @RequestBody UserDto userDto) {
+    return ResponseEntity.ok(userService.signup(userDto));
+  }
+
+  @GetMapping("/user")
+  @PreAuthorize("hasAnyRole('USER','ADMIN')")
+  public ResponseEntity<UserDto> getMyUserInfo(HttpServletRequest request) {
+    return ResponseEntity.ok(userService.getMyUserWithAuthorities());
+  }
+
+  @GetMapping("/user/{username}")
+  @PreAuthorize("hasAnyRole('USER','ADMIN')")
+  public ResponseEntity<UserDto> getUserInfo(@PathVariable String username) {
+    return ResponseEntity.ok(userService.getUserWithAuthorities(username));
+  }
+}
